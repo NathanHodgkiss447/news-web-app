2da882168aad835c6b2bf2d9683086a8c85d2e33
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2020-10-07 19:44:42 +0100

    Minor re-structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 import "./App.css";
 
+const alanKey = "";
+
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+    });
+  }, []);
+
   return (
     <div className="app">
-      <h1 className="app__content">Testing App</h1>
+      <h1 className="app__content">News Web App</h1>
     </div>
   );
 }
