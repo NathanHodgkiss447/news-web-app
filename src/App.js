488a8ab9216b70fdbc5751cb6cad134eddb4cfe4
NathanHodgkiss447 <nathanhodgkiss447@gmail.com>
2020-10-09 00:18:15 +0100

    Took in API data + added card components.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
+//48:22 video
+
+import React, { useState, useEffect } from "react";
+import { useIsFocusVisible } from "@material-ui/core";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import "./App.css";
+import NewsCards from "./components/NewsCards/NewsCards";
 
-const alanKey = "";
+const alanKey =
+  "833f3ea102af48a60d7478389dbdec362e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+        }
+      },
     });
   }, []);
 
   return (
     <div className="app">
       <h1 className="app__content">News Web App</h1>
+      <NewsCards articles={newsArticles} />
     </div>
   );
 }
